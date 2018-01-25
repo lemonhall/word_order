288022ec18e3fed66bc01296199548c935771a6d
余柠 <yuning@sf-express.com>
2018-01-25 23:20:17 +0800

    with server

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,8 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+var taskTitle   = '';
+var taskContent = '';
 
 Page({
   data: {
@@ -9,15 +11,41 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+  getTaskTitle: function (e) {
+    console.log("getTaskTitle:"+e.detail.value);
+      taskTitle = e.detail.value;
+  },
+  getTaskContent: function (e) {
+    console.log("getTaskContent:" + e.detail.value);
+      taskContent = e.detail.value;
+  },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
+  createTask:function(e){
+    console.log("onClick with button");
+    wx.request({
+      url: 'http://192.168.50.12:8080/createTask/', //仅为示例，并非真实的接口地址,
+      method:'POST',
+      data: {
+        taskTitle,
+        taskContent
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    });
+    //END of request...
+  },
   onLoad: function () {
     wx.showShareMenu({
       withShareTicket: true
-    })
+    });
   }  
 })
---
+++ server/index.js
@@ -0,0 +1,31 @@
+//express_demo.js 文件
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// 创建 application/x-www-form-urlencoded 编码解析
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+//app.configure(function () {
+//    app.use(express.bodyParser({ keepExtensions: true, uploadDir: '/tmp' }));
+//});
+
+app.use(bodyParser.json());
+ 
+app.get('/', function (req, res) {
+   console.log(req);
+   res.send('Hello World');
+})
+
+app.post('/createTask',function(req,res){
+	console.log(req.body);
+	res.send("OK I got a new Task");
+});
+var server = app.listen(8080, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
